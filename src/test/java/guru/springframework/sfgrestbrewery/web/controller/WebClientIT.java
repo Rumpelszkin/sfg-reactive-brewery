8c68032b8e5e1567ba896ccd4e65b92a27fd02d0
Rumpelszkin <kolbuszewskimax@gmail.com>
2021-08-06 12:10:58 +0200

    saveNewBeer reactive implemented

--- src/test/java/guru/springframework/sfgrestbrewery/web/controller/WebClientIT.java
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/WebClientIT.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.netty.http.client.HttpClient;
 
+import java.math.BigDecimal;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -31,18 +34,21 @@ public class WebClientIT {
     @BeforeEach
     void setUp() {
         webClient = WebClient.builder()
-                .baseUrl(BASE_URL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.create().wiretap(true)))
-                .build();
+                             .baseUrl(BASE_URL)
+                             .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                                                                                       .wiretap(true)))
+                             .build();
     }
 
     @Test
     void getBeerById() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        Mono<BeerDto> beerDtoMono = webClient.get().uri("api/v1/beer/1")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve().bodyToMono(BeerDto.class);
+        Mono<BeerDto> beerDtoMono = webClient.get()
+                                             .uri("api/v1/beer/1")
+                                             .accept(MediaType.APPLICATION_JSON)
+                                             .retrieve()
+                                             .bodyToMono(BeerDto.class);
 
         beerDtoMono.subscribe(beer -> {
             assertThat(beer).isNotNull();
@@ -59,9 +65,11 @@ public class WebClientIT {
     void getBeerByUpc() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        Mono<BeerDto> beerDtoMono = webClient.get().uri("api/v1/beerUpc/"+ BeerLoader.BEER_1_UPC)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve().bodyToMono(BeerDto.class);
+        Mono<BeerDto> beerDtoMono = webClient.get()
+                                             .uri("api/v1/beerUpc/" + BeerLoader.BEER_1_UPC)
+                                             .accept(MediaType.APPLICATION_JSON)
+                                             .retrieve()
+                                             .bodyToMono(BeerDto.class);
 
         beerDtoMono.subscribe(beer -> {
             assertThat(beer).isNotNull();
@@ -79,17 +87,73 @@ public class WebClientIT {
 
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        Mono<BeerPagedList> beerPagedListMono = webClient.get().uri("/api/v1/beer")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve().bodyToMono(BeerPagedList.class);
+        Mono<BeerPagedList> beerPagedListMono = webClient.get()
+                                                         .uri("/api/v1/beer")
+                                                         .accept(MediaType.APPLICATION_JSON)
+                                                         .retrieve()
+                                                         .bodyToMono(BeerPagedList.class);
 
-        beerPagedListMono.publishOn(Schedulers.parallel()).subscribe(beerPagedList -> {
+        beerPagedListMono.publishOn(Schedulers.parallel())
+                         .subscribe(beerPagedList -> {
 
-            beerPagedList.getContent().forEach(beerDto -> System.out.println(beerDto.toString()));
+                             beerPagedList.getContent()
+                                          .forEach(beerDto -> System.out.println(beerDto.toString()));
 
+                             countDownLatch.countDown();
+                         });
+
+        countDownLatch.await();
+    }
+
+    @Test
+    void testSavedBeer() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        BeerDto beerDto = BeerDto.builder()
+                                 .beerName("Rumpis favs")
+                                 .beerStyle("IPA")
+                                 .price(new BigDecimal("6.99"))
+                                 .upc("123123321")
+                                 .build();
+
+        Mono<ResponseEntity<Void>> responseEntityMono = webClient.post()
+                                                                 .uri("/api/v1/beer")
+                                                                 .accept(MediaType.APPLICATION_JSON)
+                                                                 .body(BodyInserters.fromValue(beerDto))
+                                                                 .retrieve()
+                                                                 .toBodilessEntity();
+        responseEntityMono.publishOn(Schedulers.parallel()).subscribe(responseEntity -> {
+            assertThat(responseEntity.getStatusCode().is2xxSuccessful());
             countDownLatch.countDown();
         });
 
-        countDownLatch.await();
+        countDownLatch.await(1000, TimeUnit.MILLISECONDS);
+        assertThat(countDownLatch.getCount()).isEqualTo(0);
+    }
+
+    @Test
+    void testSavedBeerBadRequest() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        BeerDto beerDto = BeerDto.builder()
+                                 .price(new BigDecimal("6.99"))
+                                 .upc("123123321")
+                                 .build();
+
+        Mono<ResponseEntity<Void>> responseEntityMono = webClient.post()
+                                                                 .uri("/api/v1/beer")
+                                                                 .accept(MediaType.APPLICATION_JSON)
+                                                                 .body(BodyInserters.fromValue(beerDto))
+                                                                 .retrieve()
+                                                                 .toBodilessEntity();
+
+        responseEntityMono.publishOn(Schedulers.parallel()).doOnError(throwable -> {
+            countDownLatch.countDown();
+        }).subscribe(responseEntity -> {
+
+        });
+
+        countDownLatch.await(1000, TimeUnit.MILLISECONDS);
+        assertThat(countDownLatch.getCount()).isEqualTo(0);
     }
 }
